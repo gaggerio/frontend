306ed5e1e712a706cb7adce043b0a9250edbe033
Denis Lit <denislitviak@gmail.com>
2023-06-05 21:00:55 +0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -12,17 +12,17 @@ export default defineConfig({
     watch: {
       usePolling: true
     },
-    proxy: {
-      '^/proxy': {
-        target: 'http://localhost:3100', // Update with your proxy server URL
-        changeOrigin: true,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-          'Access-Control-Allow-Headers': 'Content-Type',
-        },
-      },
-    },
+    // proxy: {
+    //   '^/proxy': {
+    //     target: 'http://localhost:3100', // Update with your proxy server URL
+    //     changeOrigin: true,
+    //     headers: {
+    //       'Access-Control-Allow-Origin': '*',
+    //       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+    //       'Access-Control-Allow-Headers': 'Content-Type',
+    //     },
+    //   },
+    // },
   },
   resolve: {
     alias: {
